5c51904e0cea6878ff6de159168ec0d215916df7
Andrew Giang <giang184@gmail.com>
2021-09-21 15:21:58 -0700

    add routes for sign in sign up

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ProjectDetails(props) {
   const id=props.match.params.id;
   return (
-    <div className="contain section project-details">
+    <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
           <span className="card-title">Project Title - {id}</span>
