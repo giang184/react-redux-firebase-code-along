259a62b8e2de281584342fb776de8f0f7810696d
Andrew Giang <giang184@gmail.com>
2021-09-21 14:46:42 -0700

    add project list template

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const ProjectList = () => {
+  return (
+    <div className='project-list section'>
+      <div className='card z-depth-0 project-summary'>
+        <div className="card-content grey-text text-dark-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Andrew Giang</p>
+          <p className="grey-text">3rd, September, 2am</p>
+        </div>
+      </div>
+      <div className='card z-depth-0 project-summary'>
+        <div className="card-content grey-text text-dark-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Andrew Giang</p>
+          <p className="grey-text">3rd, September, 2am</p>
+        </div>
+      </div>
+      <div className='card z-depth-0 project-summary'>
+        <div className="card-content grey-text text-dark-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Andrew Giang</p>
+          <p className="grey-text">3rd, September, 2am</p>
+        </div>
+      </div>
+      <div className='card z-depth-0 project-summary'>
+        <div className="card-content grey-text text-dark-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Andrew Giang</p>
+          <p className="grey-text">3rd, September, 2am</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectList;
