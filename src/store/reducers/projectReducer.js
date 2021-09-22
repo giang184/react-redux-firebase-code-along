c307e17a3b3f89cba8bdda3694134be6a6388e12
Andrew Giang <giang184@gmail.com>
2021-09-22 14:13:29 -0700

    create dummy data for projects

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+  projects: [
+    {id: '1', title: 'hello', content: "blah"},
+    {id: '2', title: 'there', content: "blah"},
+    {id: '3', title: 'goodbye', content: "blah"},
+  ]
+}
 
 const projectReducer = (state = initState, action) => {
   return state;
