6c107dfee5865efb1e30ad6a0619521bfa5e52e9
Andrew Giang <giang184@gmail.com>
2021-09-22 16:27:50 -0700

    debug config

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,8 +1,7 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCVgJFkONKpIcoL7GBFEbRNNiiw35JhIf4",
   authDomain: "tutorial-marioplan-d9f20.firebaseapp.com",
@@ -11,9 +10,9 @@ const firebaseConfig = {
   messagingSenderId: "652897030134",
   appId: "1:652897030134:web:50d847fc852ebe73bc81d7",
   measurementId: "G-8DF4YXKF5K"
-};
+}
 
 firebase.initializeApp(firebaseConfig);
-firebase.firestore().settings({timestampsInSnapshots: true});
+firebase.firestore().settings({ timestampsInSnapshots: true });
 
 export default firebase;
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,17 @@
 export const createProject = (project) => {
   return (dispatch, getState, {getFirebase, getFirestore}) => {
     //make async call to db
-    dispatch({ type: 'CREATE_PROJECT', project });
+    const firestore = getFirestore();
+    firestore.collection('projects').add({
+      ...project,
+      authorFirstName: 'temp',
+      authorLastName: 'temp last',
+      authorID: 123,
+      createdAt: new Date()
+    }).then(() => {
+      dispatch({ type: 'CREATE_PROJECT', project });
+    }).catch((err) => {
+      dispatch({type: 'CREATE-PROJECT_ERROR', err});
+    })
   }
 };
