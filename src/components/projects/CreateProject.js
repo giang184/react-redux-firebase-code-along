2e68c1566959f1539a785980970767369545f939
Andrew Giang <giang184@gmail.com>
2021-09-23 11:53:30 -0700

    add route guard

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import { createProject } from '../../store/actions/projectActions'
+import {Redirect} from 'react-router-dom'
 
 class CreateProject extends Component {
   state = {
@@ -17,6 +18,8 @@ class CreateProject extends Component {
     this.props.createProject(this.state)
   }
   render() {
+    const {auth} = this.props;
+    if(!auth.uid) return<Redirect to= '/signin' />
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -38,6 +41,11 @@ class CreateProject extends Component {
     )
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth
+  }
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -45,4 +53,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default connect(null, mapDispatchToProps)(CreateProject)
+export default connect(mapStateToProps, mapDispatchToProps)(CreateProject)
